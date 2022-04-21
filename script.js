21ddd0b5db0cda669894d1aea49f04e02636905b
Michał <michalbystryk14@wp.pl>
2022-04-21 20:34:37 +0200

    add function "getWeather"

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const API_KEY = '&appid=638c587993b9533b6d691b999b840c52';
 const API_UNITS = '&units=metric';
 
+const getWeather = () => {
+    const city = input.value;
+    const API_URL = API_LINK + city + API_KEY + API_UNITS;
+
+    axios.get(API_URL).then(res => {
+        
+    })
+}
