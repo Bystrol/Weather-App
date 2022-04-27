a6580c31a63ba1b4de49b7cdf4fdf2ba59542a1c
Michał <michalbystryk14@wp.pl>
2022-04-27 15:27:06 +0200

    change div 'parameters3', add p 'error-text'

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
+const errorText = document.querySelector('.error-text');
 const cityName = document.querySelector('.city-name');
 const list1 = document.querySelector('ul:nth-of-type(1)');
 const list2 = document.querySelector('ul:nth-of-type(2)');
@@ -7,6 +8,7 @@ const img = document.querySelector('img');
 const pressure = document.querySelector('.pressure');
 const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
+const temperature = document.querySelector('.temperature');
 
 const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const API_KEY = '&appid=638c587993b9533b6d691b999b840c52';
@@ -17,6 +19,19 @@ const getWeather = () => {
     const API_URL = API_LINK + city + API_KEY + API_UNITS;
 
     axios.get(API_URL).then(res => {
-        
-    })
+        console.log(res.data);
+        cityName.textContent = input.value;
+        pressure.textContent = res.data.main.pressure + ' hPa';
+        humidity.textContent = res.data.main.humidity + ' %';
+        temperature.textContent = Math.floor(res.data.main.temp) + ' °C';
+    }).catch(() => errorText.textContent = 'Wpisz poprawną nazwę!')
 }
+
+const enterCheck = (e) =>{
+    if(e.key === 'Enter'){
+        getWeather();
+    }
+}
+
+btn.addEventListener('click', getWeather);
+input.addEventListener('keyup', enterCheck);
